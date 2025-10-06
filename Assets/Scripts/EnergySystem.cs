da0f2018d4d3e55333997236906d0aa30e37973a
aashnakulsh <aashna.kulsh@gmail.com>
2025-10-05 22:35:33 -0400

    Energy Persistence
    Created a single instance of the energy system so that energy values would persist across scenes

--- Assets/Scripts/EnergySystem.cs
+++ Assets/Scripts/EnergySystem.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
-// EnergySystem script
+// ========== EnergySystem Script ==========
 // manages functions that can reduce or add energy
 // energy is a percentage with 100 being the maximum and 0 the minimum 
+// 
+// ---- USAGE ----
+// EnergySystem.es.<functionCall> where functionCall is any of the public functions marked below (e.g. ReduceEnergy(n) or GetCurrentEnergy())
+
 public class EnergySystem : MonoBehaviour
 {
-    public float totalEnergy = 100f;   // start value of energy
+
+    public static EnergySystem es; 
+
+    public float totalEnergy = 100f;  
     private float energy;
     private float energyDrainRate = 1f;
 
@@ -13,8 +20,20 @@ public class EnergySystem : MonoBehaviour
     // private float timeForDrain = 120f;                           // 2 minutes
     // private float altDrainRate = energy / timeForDrain;         // gives us the drain rate so that all energy will be drained after timeForDrain seconds
 
-    void Start()
+
+    private void Awake()
     {
+        if (es == null)     // if no instance of the energy system exisits, create one
+        {
+            es = this;
+            DontDestroyOnLoad(gameObject);  // ensures persistence across scenes
+        }
+        else
+        {
+            Destroy(gameObject);            // ensures only 1 instance exists
+            return;
+        }
+
         energy = totalEnergy;
     }
 
@@ -34,14 +53,19 @@ public class EnergySystem : MonoBehaviour
         }
     }
 
+    // ==============================================================
+    //                        PUBLIC FUNCTIONS
+    // ==============================================================
+
     // Decreases energy by a given number n
     // (e.g. energy is originally 80. DecreaseEnergy(15) => energy = 65)
     public void ReduceEnergy(float n)
     {
-        if (n < 0) {
+        if (n < 0)
+        {
             Debug.LogWarning("ReduceEnergy called with negative value!");
-            n = 0f;                             // do not change energy
-        } 
+            return;
+        }
         energy -= n;
         energy = Mathf.Clamp(energy, 0f, totalEnergy);
     }
@@ -50,10 +74,11 @@ public class EnergySystem : MonoBehaviour
     // (e.g. energy is originally 80. IncreaseEnergy(15) => energy = 95)
     public void IncreaseEnergy(float n)
     {
-        if (n < 0) {
+        if (n < 0)
+        {
             Debug.LogWarning("IncreaseEnergy called with negative value!");
-            n = 0f;                             // do not change energy
-        } 
+            return;
+        }
         energy += n;
         energy = Mathf.Clamp(energy, 0f, totalEnergy);
     }
@@ -61,10 +86,11 @@ public class EnergySystem : MonoBehaviour
     // Sets a new drain rate
     public void SetDrainRate(float newDrainRate)
     {
-        if (newDrainRate < 0) {
+        if (newDrainRate < 0)
+        {
             Debug.LogWarning("newDrainRate called with negative value!");
-            newDrainRate = energyDrainRate;     // do not change drain rate
-        } 
+            return;
+        }
         energyDrainRate = newDrainRate;
     }
 
