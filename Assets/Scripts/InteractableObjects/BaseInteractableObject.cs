ff0dfd871d3c8fb222cc94750ca3c9f2a5b0d1ab
Zachary L <zachary.lebak@gmail.com>
2025-10-12 17:12:38 -0400

    Interaction System
    - Added a interactable interface that is added to any interactable object
    - Added a base interactable object script that can be extended to add an interact feature
    - Added a material for when an interactable object is "selected", meaning it is the closest one and can be interacted with
    - Modified player input system actions to remove hold modifier for interact

---
+++ Assets/Scripts/InteractableObjects/BaseInteractableObject.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BaseInteractableObject : MonoBehaviour, I_Interactable
+{
+    // Variables
+    [SerializeField] private GameObject selectedVisual;
+    private bool isSelected;
+
+    // Start
+    private void Start()
+    {
+        selectedVisual.SetActive(false); //hide the selected visual
+    }
+
+    // Interact
+    public virtual void Interact()
+    {
+        Debug.LogWarning("Base Interactable Object");
+    }
+
+    // Set Selected
+    public void SetSelected(bool isSelected)
+    {
+        this.isSelected = isSelected;
+
+        // enable/disable the visual based on if it's selected
+        if (this.isSelected) { selectedVisual.SetActive(true); }
+        else { selectedVisual.SetActive(false); }
+    }
+
+    // Is Selected
+    public bool IsSelected()
+    {
+        return isSelected;
+    }
+
+    // Get Transform
+    public Transform GetTransform()
+    {
+        return this.transform;
+    }
+}
